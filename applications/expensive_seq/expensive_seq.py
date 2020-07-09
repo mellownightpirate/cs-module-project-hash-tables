d8eeb35c0fd9b67fb7a7ab8289e4f35743aa4b69
mellownightpirate <a@hasan.co>
2020-07-09 14:59:31 +0100

    expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,24 @@
 # Your code here
+import time
+cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
+    # use a tuple as the Key
+    if (x, y, z) in cache.keys():
+        return cache[(x, y, z)]
 
+    result = None
+    if x <= 0:
+        result = y + z
+    if x > 0:
+        result = expensive_seq(
+            x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+
+    # use a tuple as the Key
+    cache[(x, y, z)] = result
+    return result
 
 
 if __name__ == "__main__":
